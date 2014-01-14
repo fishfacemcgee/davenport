d220ae6de82450c7e30afa0959f4535c1fe2efc3
fishfacemcgee <bgrambo@gmail.com>
2014-01-13 21:11:59 -0500

    Stub Out High Level MVP Methods for Desk API Handler

--- Davenport/DeskApi/DeskHandler.cs
+++ Davenport/DeskApi/DeskHandler.cs
@@ -10,9 +10,58 @@ namespace DeskApi
 {
     public class DeskHandler
     {
+
+        private const String ENDPOINT = "/api/v2/";
+        private const String PROTOCOL = "https://";
+
         public DeskHandler()
         {
-            Debug.WriteLine("Hello World");
+            Debug.WriteLine(PROTOCOL + "notarealurl.desk.com" + ENDPOINT);
+        }
+
+        public void getCases()
+        {
+            // We may need to call getMessage from here in order to ensure we
+            // have content to preview with. More API Doc research necessary
+        }
+
+        public void getMessage(int caseId)
+        {
+
+        }
+
+        public void getReplies(int caseId)
+        {
+
+        }
+
+        public void createReply(int caseId)
+        {
+
+        }
+
+        public void createNote(int caseId)
+        {
+
+        }
+
+        public void listAttachments(int caseId)
+        {
+
+        }
+
+        public void createAttachment(int caseId)
+        {
+
+        }
+
+        public void updateCase(int caseId)
+        {
+            /* 
+             * This will likely need to take an Array/Object containing values
+             * or a JSON string containing the data already prepared
+             * 
+             */
         }
     }
 }
