27a6e07d0c07e7990a9b84e9db899b21b5c12290
fishfacemcgee <bgrambo@gmail.com>
2014-02-08 22:51:52 -0500

    Cleanup BaseModel Spacing

--- Davenport/DeskApi/Models/BaseModel.cs
+++ Davenport/DeskApi/Models/BaseModel.cs
@@ -8,15 +8,15 @@ namespace DeskApi.Models
 {
     class BaseModel
     {
-        int Id { get; private set; }
-        string External_Id { get; private set; }
+        int                                 Id { get; private set; }
+        string                              External_Id { get; private set; }
         // TODO: Investigate necessity of creating Custom Field class
-        List<Dictionary<string, string>> Custom_Fields { get; private set; }
+        List<Dictionary<string, string>>    Custom_Fields { get; private set; }
         // TODO: Investigate necessity of creating Links class(es)
-        List<Dictionary<string, string>> Links { get; private set; }
+        List<Dictionary<string, string>>    Links { get; private set; }
         // Date fields (need to check if JSON.NET can safely convert into .NET-friendly date values
-        string Created_At { get; private set; }
-        string Updated_At { get; private set; }
-        string Locked_Until { get; private set; }
+        string                              Created_At { get; private set; }
+        string                              Updated_At { get; private set; }
+        string                              Locked_Until { get; private set; }
     }
 }
